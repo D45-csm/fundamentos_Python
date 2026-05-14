06e4e0dc0524fe1fce0c7bc5d21720ae9f0373c2
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-05-14 12:33:38 -0500

    tuplas, ejercicios con tuplas y organizacion de carpeta de ejercicios_actividades

---
+++ ejercicios_actividades/ejercicios_tuplas.py
@@ -0,0 +1,11 @@
+#ejercicio 1
+vector_1=(1,2,3)
+vector_2=(-1,0,2)
+print(vector_1)
+print(vector_2)
+
+#ejercicio 2
+precios=(50, 75, 46, 22, 80, 65, 8,)
+print(f"el menor de todos: {min(precios)}")
+print(f"el mayor de todos: {max(precios)}")
+
